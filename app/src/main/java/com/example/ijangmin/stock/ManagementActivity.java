fd5ce2041987fca1f3f9e47fcb142d6454010c4a
doozi316 <doozi316@gmail.com>
2019-05-19 17:40:15 +0900

    content 검색, 삭제 기능 추가

--- app/src/main/java/com/example/ijangmin/stock/ManagementActivity.java
+++ app/src/main/java/com/example/ijangmin/stock/ManagementActivity.java
@@ -3,7 +3,10 @@ package com.example.ijangmin.stock;
 import com.example.ijangmin.stock.R;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.content.Intent;
@@ -20,15 +23,36 @@ import java.util.List;
  */
 
 public class ManagementActivity extends AppCompatActivity {
+    private ListView listView;
+
+    private ContentListAdapter adapter;
+
+    private List<Content> contentList;
+
+    private List<Content> saveList;//회원검색 기능용 복사본
+
+
     public void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
-        setContentView(R.layout.table); Intent intent = getIntent();
+        setContentView(R.layout.table);
+        Intent intent = getIntent();
 
         ListView listView = (ListView) findViewById(R.id.listView);
-        ArrayList<Content> contentList = new ArrayList<Content>();
+//        ArrayList<Content> contentList = new ArrayList<Content>();
+
+
+        contentList = new ArrayList<Content>();
+
+        saveList = new ArrayList<Content>();
+
+
+
+
 
         //어댑터 초기화부분 userList와 어댑터를 연결해준다.
-        ContentListAdapter adapter = new ContentListAdapter(getApplicationContext(), contentList);
+       // ContentListAdapter adapter = new ContentListAdapter(getApplicationContext(), contentList);
+        adapter = new ContentListAdapter(getApplicationContext(), contentList, this, saveList);//로 수정됨
+
         listView.setAdapter(adapter);
 
         try{
@@ -39,7 +63,7 @@ public class ManagementActivity extends AppCompatActivity {
             JSONArray jsonArray = jsonObject.getJSONArray("response");
             int count = 0;
 
-            String content,num, conManu, date;
+            String content, num, conManu, date;
 
             //JSON 배열 길이만큼 반복문을 실행
             while(count < jsonArray.length()){
@@ -63,9 +87,51 @@ public class ManagementActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+        EditText search = (EditText)findViewById(R.id.search);
 
+        search.addTextChangedListener(new TextWatcher() {
 
+            @Override
 
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                searchContent(charSequence.toString());//회원 검색 기능용
+
+            }
+
+            @Override
+
+            public void afterTextChanged(Editable editable) {
+
+            }
+
+        });
 
     }
+
+    public void searchContent(String search){
+
+        contentList.clear();
+
+        for(int i = 0; i < saveList.size(); i++){
+
+            if(saveList.get(i).getNum().contains(search)){//contains메소드로 search 값이 있으면 true를 반환함
+
+                contentList.add(saveList.get(i));
+
+            }
+
+        }
+
+        adapter.notifyDataSetChanged();//어댑터에 값일 바뀐것을 알려줌
+
+    }
+
 }
+
