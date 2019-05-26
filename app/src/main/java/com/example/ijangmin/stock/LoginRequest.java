4d26672965d240e79af27028311cba09988ed15f
doozi316 <doozi316@gmail.com>
2019-05-26 18:58:20 +0900

    ip 주소 변경

--- app/src/main/java/com/example/ijangmin/stock/LoginRequest.java
+++ app/src/main/java/com/example/ijangmin/stock/LoginRequest.java
@@ -13,7 +13,7 @@ public class LoginRequest extends StringRequest {
 
     //현재 안드로이드앱을 에뮬레이터로 돌리므로 에뮬레이터가 설치된 서버에 있는 아파치 서버에 접근하려면
     //다음과 같이 10.0.2.2:포트번호 로 접근해야합니다 저는 8080 포트를 써서 다음과 같이 했습니다
-    final static private String URL = "http://10.0.2.2:8080/Login.php";
+    final static private String URL = "http://192.168.35.233:8080/Login.php";
     private Map<String, String> parameters;
 
     //생성자
